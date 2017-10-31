7b5b66d00c16325fdb7d006aa0096c271ab259d8
Emily Morgan <emorgan05@gmail.com>
2017-10-31 14:21:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Emily"
+  end
+
+  get '/hometown' do
+    "My hometown is Cary, NC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Blank Space by Taylor Swift"
+  end
 end
